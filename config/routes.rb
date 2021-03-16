4ddabae26ec08369f3b9dc1e7909f7da5ec31718
chiita <ec2-user@ip-172-31-90-221.ec2.internal>
2021-03-16 07:55:39 +0000

    copy bookers2

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: "homes#home"
+  # get 'users/index'
+  get 'home' => 'homes#home'
+  get 'home/about' => "homes#about"
+  devise_for :users
+  # root to: "books#index"
+  resources :books
+
+  resources :users, only: [:index, :show, :edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
