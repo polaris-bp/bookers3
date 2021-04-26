eee8c9bf9fef4ff940d40b71a211d1acf29b6806
chiita <ec2-user@ip-172-31-90-221.ec2.internal>
2021-04-26 04:56:00 +0000

    Set Refile.secret_key.

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'de12a4c4c6ffae1a91262c4853c0fbf79acd666ad6ac538891160b51c60d6e8b2029af40ce7f03a0f4597e7f8dd5779a635cd4812533acb744d5ac6720149d49'
