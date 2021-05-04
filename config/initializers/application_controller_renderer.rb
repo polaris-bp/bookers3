47827955acc9a36b9695d0ef77b72645b4881794
chiita <ec2-user@ip-172-31-90-221.ec2.internal>
2021-05-04 14:02:34 +0000

    Remove cache.

--- config/initializers/application_controller_renderer.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
-Refile.secret_key = 'de12a4c4c6ffae1a91262c4853c0fbf79acd666ad6ac538891160b51c60d6e8b2029af40ce7f03a0f4597e7f8dd5779a635cd4812533acb744d5ac6720149d49'
